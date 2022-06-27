960cb9f6d5b1b100d97dff31f42fd02773055365
sebbycake <leehur_sebastian@hotmail.com>
2022-06-27 14:02:34 +0800

    Added multiple line charts for specific order types, and configured chart options for pie and line

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,7 @@ import * as React from "react"
 
 // charts components import
 import { Chart } from 'react-chartjs-2';
-import { Chart as ChartJS , registerables } from 'chart.js'
+import { Chart as ChartJS, registerables } from 'chart.js'
 
 // local components import
 import Header from "../components/header/header"
@@ -16,11 +16,11 @@ ChartJS.register(...registerables)
 // data from API
 
 const metricsData = [
-  {stats: 580, growth_rate: 0.25},
-  {stats: 300, growth_rate: 0.15},
-  {stats: 250, growth_rate: 0.07},
-  {stats: 600, growth_rate: 0.05},
-  {stats: 105, growth_rate: -0.10},
+  { stats: 580, growth_rate: 0.25 },
+  { stats: 300, growth_rate: 0.15 },
+  { stats: 250, growth_rate: 0.07 },
+  { stats: 600, growth_rate: 0.05 },
+  { stats: 105, growth_rate: -0.10 },
 ]
 
 const total_order_card = {
@@ -37,49 +37,125 @@ const cancelled_order_card = {
   offlineOrders: "1,000"
 }
 
-// pie chart
-const orders_data = {
-  labels: [
-      'Online',
-      'Offline',
-  ],
-  datasets: [{
-      data: [65, 35],
-      backgroundColor: [
-          '#043776',
-          '#53B7E8',
-      ],
+const renderOrdersData = (data_list) => {
+  const orders_data = {
+    labels: ['Online', 'Offline'],
+    datasets: [{
+      data: data_list,
+      backgroundColor: ['#043776', '#53B7E8'],
       hoverOffset: 4
-  }]
-};
+    }]
+  }
+  return orders_data
+}
 
 const pie_chart_options = {
-    responsive: true,
-    aspectRatio: 1.2,
-    maintainAspectRatio: true,
-    plugins: {
-        legend: {
-            position: 'bottom',
-            labels: {
-                boxWidth: 12,
-                boxHeight: 12
-            }
-        }
+  responsive: true,
+  aspectRatio: 1.2,
+  maintainAspectRatio: true,
+  plugins: {
+    legend: {
+      position: 'bottom',
+      labels: {
+        boxWidth: 12,
+        boxHeight: 12
+      }
     },
+    tooltip: {
+      callbacks: {
+        label: (context) => {
+          return `${context.parsed}%`
+        }
+      }
+    }
+  },
 }
 
 
 // -------------------------------
 
-const specific_order_data = {
+const specific_order_type_data = {
   labels: ['Q2 2021', 'Q3 2021', 'Q4 2021', 'Q1 2022'],
-  datasets: [{
-    label: 'My First dataset',
-    backgroundColor: 'rgb(255, 99, 132)',
-    borderColor: 'rgb(255, 99, 132)',
-    data: [73, 75, 74, 77],
-  }]
+  datasets: [
+    {
+      label: 'Smart Hands',
+      backgroundColor: 'rgb(255, 99, 132)',
+      borderColor: 'rgb(255, 99, 132)',
+      data: [73, 75, 74, 77],
+    },
+    {
+      label: 'Cross Connect',
+      backgroundColor: '#085f63',
+      borderColor: '#085f63',
+      data: [77, 78, 78, 78],
+      hidden: true,
+    },
+    {
+      label: 'Fabric Port',
+      backgroundColor: '#eac100',
+      borderColor: '#eac100',
+      data: [42, 42, 44, 46],
+      hidden: true,
+    },
+    {
+      label: 'Equinix Connect',
+      backgroundColor: '#1e2a78',
+      borderColor: '#1e2a78',
+      data: [10, 9, 10, 13],
+      hidden: true,
+    },
+    {
+      label: 'Internet Exchange',
+      backgroundColor: '#5d5d5a',
+      borderColor: '#5d5d5a',
+      data: [16, 17, 10, 10],
+      hidden: true,
+    },
+    {
+      label: 'Metro Connect',
+      backgroundColor: '#2f89fc',
+      borderColor: '#2f89fc',
+      data: [49, 45, 46, 50],
+      hidden: true,
+    },
+  ] // end of datasets key
 };
+
+const line_chart_options = {
+  responsive: true,
+  aspectRatio: 1.5,
+  maintainAspectRatio: true,
+  plugins: {
+    legend: {
+      position: 'bottom',
+    },
+    tooltip: {
+      callbacks: {
+        label: (context) => {
+          console.log(context)
+          return `${context.raw}%`
+        }
+      }
+    }
+  },
+  scales: {
+    x: {
+      grid: {
+        display: false
+      }
+    },
+    y: {
+      grid: {
+        display: false
+      },
+      ticks: {
+        callback: (value, index, values) => {
+          return `${value}%`
+        }
+      }
+    }
+  }
+}
 
 // markup
 const IndexPage = () => {
@@ -90,18 +166,18 @@ const IndexPage = () => {
 
       <MetricTitle title="Quarterly Overview" />
 
-      <MetricCardList data={metricsData}/>
+      <MetricCardList data={metricsData} />
 
       <section className="orders-card-container">
 
         <div className="orders-grid-container">
           <MetricTitle title="Online Orders By Category" />
           <div className="orders-charts">
-            <SelectorBtnList />
+            {/* <SelectorBtnList /> */}
             <div class="orders-charts-container">
               <div class="orders-combo-chart">
                 {/* <canvas id="online-orders-combo-chart"></canvas> */}
-                <Chart type="line" data={specific_order_data} />
+                <Chart type="line" data={specific_order_type_data} options={line_chart_options} />
               </div>
             </div>
           </div> {/* end of orders charts */}
@@ -109,13 +185,12 @@ const IndexPage = () => {
 
         <div class="orders-metric-card-a">
           <MetricTitle title="Orders By Channel" />
-          <OrderCard order_card={total_order_card} chart={<Chart type="pie" data={orders_data} options={pie_chart_options} />} />
+          <OrderCard order_card={total_order_card} chart={<Chart type="pie" data={renderOrdersData([65, 35])} options={pie_chart_options} />} />
         </div>
-
 
         <div class="orders-metric-card-b">
           <MetricTitle title="Cancelled Orders By Channel" />
-          <OrderCard order_card={cancelled_order_card} chart={<Chart type="pie" data={orders_data} options={pie_chart_options} />} />
+          <OrderCard order_card={cancelled_order_card} chart={<Chart type="pie" data={renderOrdersData([80, 20])} options={pie_chart_options} />} />
         </div>
 
       </section>
