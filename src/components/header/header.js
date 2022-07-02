dca6995b07e7ce31e449467448bfe4e2b82b51a5
sebbycake <leehur_sebastian@hotmail.com>
2022-07-02 20:45:11 +0800

    Changed dropdown btn to select tag

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,6 +1,5 @@
 import React from "react"
 import equinix_logo from "../../images/equinix_logo.png"
-import down_arrow from "../../images/icons/down-arrow.png"
 import * as styles from "./header.module.css"
 
 const Header = () => {
@@ -10,10 +9,12 @@ const Header = () => {
             <div>
                 <img className={styles.logo} src={equinix_logo} alt="Equinix Logo" />
             </div>
-            <div className={styles.dropdownBtn}>
-                <div className={styles.btnText}>Q1 2022</div>
-                <img className={styles.downArrow} src={down_arrow} alt="Down Arrow Icon" />
-            </div>
+            <select className={styles.dropdownBtn}>
+                <option className={styles.options} value="Q1 2022">Q1 2022</option>
+                <option className={styles.options} value="Q4 2021">Q4 2021</option>
+                <option className={styles.options} value="Q3 2021">Q3 2021</option>
+                <option className={styles.options} value="Q2 2021">Q2 2021</option>
+            </select>
         </div>
     )
 
