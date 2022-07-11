abc4d80f835ba0258b9fb0aa10a645b49bc66432
sebbycake <leehur_sebastian@hotmail.com>
2022-07-11 13:47:11 +0800

    Handled loading and error API calls cases for metrics card list

--- src/components/metric-card-list/metric-card-list.js
+++ src/components/metric-card-list/metric-card-list.js
@@ -8,25 +8,23 @@ import commsIcon from "../../images/icons/communication.png"
 import reportIcon from "../../images/icons/report.png"
 import notificationIcon from "../../images/icons/notification.png"
 
-import data from '../../dummy-data/metrics-data'
-
 import { useUsersQuery, useCommsQuery, useReportsQuery, useNotificationsQuery } from "../../queries"
 
 const MetricCardList = ({ quarter }) => {
 
-    const { data: users } = useUsersQuery()
-    const { data: comms } = useCommsQuery()
-    const { data: reports } = useReportsQuery()
-    const { data: notifications } = useNotificationsQuery()
+    const { status: usersStatus, data: users } = useUsersQuery()
+    const { status: commsStatus, data: comms } = useCommsQuery()
+    const { status: reportsStatus, data: reports } = useReportsQuery()
+    const { status: notificationsStatus, data: notifications } = useNotificationsQuery()
 
     return (
         <div>
             <MetricTitle title="Quarterly Overview" />
             <div className={styles.metricsGrid}>
-                {/* <MetricCard title="Users" icon={userIcon} stats={users.data.data[quarter].userAccounts} growth_rate={users.data.data[quarter].growthRate} />
-                <MetricCard title="2-way Comms" icon={commsIcon} stats={comms.data.data[quarter].totalNumber} growth_rate={comms.data.data[quarter].growthRate} />
-                <MetricCard title="Reports" icon={reportIcon} stats={reports.data.data[quarter].reports} growth_rate={reports.data.data[quarter].growthRate } />
-                <MetricCard title="Notifications" icon={notificationIcon} stats={notifications.data.data[quarter].notifications} growth_rate={notifications.data.data[quarter].growthRate} /> */}
+                <MetricCard title="Users" icon={userIcon} status={usersStatus} data={users} quarter={quarter} key="userAccounts" />
+                <MetricCard title="2-way Comms" icon={commsIcon} status={commsStatus} data={comms} quarter={quarter} key="totalNumber" />
+                <MetricCard title="Reports" icon={reportIcon} status={reportsStatus} data={reports} quarter={quarter} key="reports" />
+                <MetricCard title="Notifications" icon={notificationIcon} status={notificationsStatus} data={notifications} quarter={quarter} key="notifications" />
             </div>
         </div>
     )
--- src/components/metric-card/metric-card.js
+++ src/components/metric-card/metric-card.js
@@ -1,30 +1,54 @@
 import React from "react";
-import caretUpIcon from "../../images/icons/caret-arrow-up.png"
-import caretDownIcon from "../../images/icons/caret-arrow-down.png"
-import * as styles from "./metric-card.module.css"
+import caretUpIcon from "../../images/icons/caret-arrow-up.png";
+import caretDownIcon from "../../images/icons/caret-arrow-down.png";
+import * as styles from "./metric-card.module.css";
 
-const MetricCard = ({title, icon, stats, growth_rate}) => {
+const MetricCard = ({ title, icon, status, data, quarter, key }) => {
+	let dataRetrieved;
 
-    return (
-        <div className={styles.metricCard}>
-            <div className={styles.metricIconContainer}>
-                <div className={styles.iconContainer}>
-                    <img className={styles.userIcon} src={icon} alt="" />
-                </div>
-                <div className={styles.metricIconName}>{title}</div>
-            </div>
-            <div className={styles.metricDetailsContainer}>
-                <div className={styles.metricNumber}>{stats}</div>
-                <div className={styles.metricGrowthContainer}>
-                    <div>
-                        <img className={styles.caretUpIcon} src={growth_rate > 0 ? caretUpIcon : caretDownIcon} alt="" />
-                    </div>
-                    <div className={styles.metricGrowthRate}>{`${Math.abs(Math.round(growth_rate * 100))}%`}</div>
-                </div>
-            </div>
-        </div>
-    )
+	if (status === "success") {
+		dataRetrieved = data.data.data;
+	}
 
-} // end of function
+	return (
+		<div className={styles.metricCard}>
+			<div className={styles.metricIconContainer}>
+				<div className={styles.iconContainer}>
+					<img className={styles.userIcon} src={icon} alt="" />
+				</div>
+				<div className={styles.metricIconName}>{title}</div>
+			</div>
+			<div className={styles.metricDetailsContainer}>
+				{status === "loading" ? (
+					"Loading data..."
+				) : status === "error" ? (
+					"Error retrieving data"
+				) : (
+					<div>
+						<div className={styles.metricNumber}>
+							{dataRetrieved[quarter][key]}
+						</div>
+						<div className={styles.metricGrowthContainer}>
+							<div>
+								<img
+									className={styles.caretUpIcon}
+									src={
+										dataRetrieved[quarter].growthRate > 0
+											? caretUpIcon
+											: caretDownIcon
+									}
+									alt=""
+								/>
+							</div>
+							<div className={styles.metricGrowthRate}>{`${Math.abs(
+								Math.round(dataRetrieved[quarter].growthRate * 100)
+							)}%`}</div>
+						</div>
+					</div>
+				)}
+			</div>
+		</div>
+	);
+}; // end of function
 
-export default MetricCard
+export default MetricCard;
